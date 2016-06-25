8bad0a3db2367710fdf4d1caa044d867a4ad240d
Centimitr <Centimitr@gmail.com>
2016-06-25 10:27:46 +0800

    update

--- namespace.go
+++ namespace.go
@@ -1,34 +1,48 @@
+// Copyright 2016 Cyako Author
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required` by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package namespace
 
-type namespace struct {
+type Namespace struct {
 	scopes  map[string]scope
 	getRule func(scope, name string) string
 }
 
-type scope struct {
+type Scope struct {
 	// scope's name
 	name string
 	// the namespace it belongs to
-	namespace         *namespace
+	namespace         *Namespace
 	getRule           func(scope, name string) string
 	UseDefaultGetRule bool
 }
 
-func (n *namespace) Apply(scopeName string) (ok bool, _ scope) {
+func (n *Namespace) Apply(scopeName string) (ok bool, _ Scope) {
 	if _, ok := n.scopes[scopeName]; !ok {
-		n.scopes[scopeName] = scope{
+		n.scopes[scopeName] = Scope{
 			name:              scopeName,
 			namespace:         n,
 			UseDefaultGetRule: true,
 		}
 		return true, n.scopes[scopeName]
 	}
-	return false, scope{}
+	return false, Scope{}
 }
 
-func (n *namespace) Use(scopeName string) scope {
+func (n *Namespace) Use(scopeName string) Scope {
 	if _, ok := n.scopes[scopeName]; !ok {
-		n.scopes[scopeName] = scope{
+		n.scopes[scopeName] = Scope{
 			name:              scopeName,
 			namespace:         n,
 			UseDefaultGetRule: true,
@@ -37,7 +51,7 @@ func (n *namespace) Use(scopeName string) scope {
 	return n.scopes[scopeName]
 }
 
-func (n *namespace) SetGetRule(fn func(string, string) string) {
+func (n *Namespace) SetGetRule(fn func(string, string) string) {
 	n.getRule = fn
 }
 
@@ -54,7 +68,7 @@ func (s *scope) Get(name string) string {
 	}
 }
 
-func (n *namespace) DirectGet(scope, name string) string {
+func (n *Namespace) DirectGet(scope, name string) string {
 	if s, ok := n.scopes[scope]; ok {
 		return s.getRule(scope, name)
 	} else {
@@ -62,9 +76,16 @@ func (n *namespace) DirectGet(scope, name string) string {
 	}
 }
 
-func New() namespace {
-	return namespace{
-		scopes: make(map[string]scope),
+func (n *Namespace) Init() {
+	n.scopes = make(map[string]Scope)
+	n.getRule = func(scope, name string) string {
+		return scope + "." + name
+	}
+}
+
+func New() Namespace {
+	return Namespace{
+		scopes: make(map[string]Scope),
 		getRule: func(scope, name string) string {
 			return scope + "." + name
 		},
